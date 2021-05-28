d04ed35d0cfef13c4129dc6b117e2c86712c7018
Tim Downey <tdowney@vmware.com>
2021-05-28 14:23:02 -0700

    CI-FIX: Update bump-vendir task to copy .git files
    [#177863590](https://www.pivotaltracker.com/story/show/177863590)
    
    Authored-by: Tim Downey <tdowney@vmware.com>

--- ci/tasks/bump-vendir/task.sh
+++ ci/tasks/bump-vendir/task.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 tag="$(cat github-release/tag)"
+echo "Updating vendir.yml to use ${tag}"
 pushd component/build > /dev/null
   sed -i "s|ref:.*|ref: $tag|" vendir.yml
 popd > /dev/null
 
-cp -r component/* updated-component
+cp -R component/. updated-component
